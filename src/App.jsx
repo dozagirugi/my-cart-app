068a0bafbd76d06e4ddbdbd87f17c088b72c5479
dozagirugi <dozagirugi@gmail.com>
2024-06-26 16:08:26 +0900

    useRef로 비밀번호 숨기기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./components/Home/HomePage";
 import SingleProductPage from "./components/SingleProduct/SingleProductPage";
 import CartPage from "./components/Cart/CartPage";
 import MyOrderPage from "./components/MyOrder/MyOrderPage";
+import LoginPage from "./components/Authentication/LoginPage";
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
         {/* <HomePage /> */}
         {/* <SingleProductPage /> */}
         {/* <CartPage /> */}
-        <MyOrderPage />
+        {/* <MyOrderPage /> */}
+        <LoginPage />
       </main>
     </div>
   );
