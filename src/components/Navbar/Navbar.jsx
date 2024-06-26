3b947d010302a41b246fb6374c14d4d65d2787fe
dozagirugi <dozagirugi@gmail.com>
2024-06-26 09:06:45 +0900

    검색창 만들기

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
 import "./Navbar.css";
 
 const Navbar = () => {
-  <nav className="align-center navbar">
-    <div className="align_center">
-      <h1 className="navbar_heading">myCart</h1>
-      <form className="align_center navbar_form">
-        <nput type="text" className="navbar_search" placeholder="제품 찾기" />
-        <button type="submit" className="search_button">
-          검색
-        </button>
-      </form>
-    </div>
+  return (
+    <nav className="align-center navbar">
+      <div className="align_center">
+        <h1 className="navbar_heading">My Cart</h1>
+        <form className="align_center navbar_form">
+          <nput type="text" className="navbar_search" placeholder="제품 찾기" />
+          <button type="submit" className="search_button">
+            검색
+          </button>
+        </form>
+      </div>
 
-    <div className="align_center navbar_links" />
-  </nav>;
+      <div className="align_center navbar_links" />
+    </nav>
+  );
 };
 
 export default Navbar;
